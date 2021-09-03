cd7438ed9dd1879eae1012cb60c5e9832403e7ab
surajhetc@gmail.com <surajhetc@gmail.com>
2021-09-03 13:35:20 +0530

    3rd commit, Added comments to source code

--- app.js
+++ app.js
@@ -2,36 +2,43 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+         // mongoose connection establishment
 const mongoose=require("mongoose");
-
 mongoose.connect('mongodb://localhost:27017/chatbox');
-
-
 mongoose.connection.on("error",(err)=>{
     console.log("MongoDB connection error!!!"+err.message)
 });
 mongoose.connection.once("open",()=>{
     console.log("MongoDB connected")});
 
+
+            //creating the message schema and model
 const { Schema } = mongoose;
 const msgSchema=new Schema({name:String,msg:String});
-
 const msg=mongoose.model("msg",msgSchema);
 
+
+            //routing the get request
 app.get('/', function(req, res){
    res.sendFile(__dirname+'/index.html')});
    
    io.on('connection', function(socket){
       console.log('A user connected');
       
-
+      //displaying al the stored messages
       msg.find((err,msgs)=>{
          socket.emit('load',msgs);
       })
       
+      //displaying all the real time messages if sent
       socket.on('msg', function(data){
+
+         //storing the message in database
          const newmsg= new msg({name:data.name,msg:data.msg});
          newmsg.save();
+
+         //senting a real time message
          io.sockets.emit('newmsg', data);
       })
    });
